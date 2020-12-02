4e55927b9a61b2006eda8fce8b670f223c0742e0
VHS <listas.vhs@gmail.com>
2020-12-02 15:44:30 -0300

    change roles creation order.

--- titanic-init.sql
+++ titanic-init.sql
@@ -1,19 +1,7 @@
 -- Connect to DATABASE titanicdb; 
 \c titanicdb 
 
--- Create some roles (read-only and read-write)
-CREATE ROLE api_ro NOLOGIN;
-grant usage on schema public to api_ro;
-grant select on public.people to api_ro;
 
-CREATE ROLE api_rw NOLOGIN;
-grant usage on schema public to api_rw;
-grant all on public.people to api_rw;
-
--- Associate the previous roles with an authenticator.
-CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'SDldYw7G2FxnzKCeoM3TPhGSGW1l9bEz';
-grant api_ro to authenticator;
-grant api_rw to authenticator;
 
 -- Add a extension to create uuid
 -- https://www.postgresql.org/docs/current/uuid-ossp.html
@@ -33,6 +21,22 @@ CREATE TABLE IF NOT EXISTS people (
 	"fare" numeric,
     PRIMARY KEY (uuid)
 );
+
+
+-- Create some roles (read-only and read-write)
+CREATE ROLE api_ro NOLOGIN;
+grant usage on schema public to api_ro;
+grant select on public.people to api_ro;
+
+CREATE ROLE api_rw NOLOGIN;
+grant usage on schema public to api_rw;
+grant all on public.people to api_rw;
+
+-- Associate the previous roles with an authenticator.
+CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'SDldYw7G2FxnzKCeoM3TPhGSGW1l9bEz';
+grant api_ro to authenticator;
+grant api_rw to authenticator;
+
 
 -- Copy data from CSV file cloned from repository in InitContainer
 COPY people (survived,passengerClass,name,sex,age,siblingsOrSpousesAboard,parentsOrChildrenAboard,fare) FROM '/docker-entrypoint-initdb.d/titanic.csv' DELIMITER ',' CSV HEADER;
