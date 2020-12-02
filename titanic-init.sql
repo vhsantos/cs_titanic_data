293909442cfbfe93ddf7e63337510c153d477e49
VHS <listas.vhs@gmail.com>
2020-12-02 15:19:10 -0300

    Add roles and fix the column name (case-sensitive).

--- titanic-init.sql
+++ titanic-init.sql
@@ -1,6 +1,19 @@
--- Create and connect to DB
--- CREATE DATABASE titanicdb; 
+-- Connect to DATABASE titanicdb; 
 \c titanicdb 
+
+-- Create some roles (read-only and read-write)
+CREATE ROLE api_ro NOLOGIN;
+grant usage on schema public to api_ro;
+grant select on public.people to api_ro;
+
+CREATE ROLE api_rw NOLOGIN;
+grant usage on schema public to api_rw;
+grant all on public.people to api_rw;
+
+-- Associate the previous roles with an authenticator.
+CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'SDldYw7G2FxnzKCeoM3TPhGSGW1l9bEz';
+grant api_ro to authenticator;
+grant api_rw to authenticator;
 
 -- Add a extension to create uuid
 -- https://www.postgresql.org/docs/current/uuid-ossp.html
@@ -10,20 +23,19 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Create a table people with a uuid primary key generate automaticly
 CREATE TABLE IF NOT EXISTS people (
 	uuid uuid DEFAULT uuid_generate_v4 (),
-	survived boolean,
-	passengerClass integer NOT NULL,
-    name VARCHAR NOT NULL,
-    sex VARCHAR NOT NULL,
-	age numeric,
-	siblingsOrSpousesAboard integer,
-	parentsOrChildrenAboard integer,
-	fare numeric,
+	"survived" boolean,
+	"passengerClass" integer NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "sex" VARCHAR NOT NULL,
+	"age" numeric,
+	"siblingsOrSpousesAboard" integer,
+	"parentsOrChildrenAboard" integer,
+	"fare" numeric,
     PRIMARY KEY (uuid)
 );
 
 -- Copy data from CSV file cloned from repository in InitContainer
-COPY people (survived,passengerclass,name,sex,age,siblingsorspousesaboard,parentsorchildrenaboard,fare) FROM '/docker-entrypoint-initdb.d/titanic.csv' DELIMITER ',' CSV HEADER;
-
+COPY people (survived,passengerClass,name,sex,age,siblingsOrSpousesAboard,parentsOrChildrenAboard,fare) FROM '/docker-entrypoint-initdb.d/titanic.csv' DELIMITER ',' CSV HEADER;
 
 -- Data sample:
 -- Survived,Pclass,Name,Sex,Age,Siblings/Spouses Aboard,Parents/Children Aboard,Fare
